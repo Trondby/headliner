2c80ad44aba10f2c0a7ee4bb2db89f9a9812126c
Trondby <Trondby@users.noreply.github.com>
2021-07-08 22:59:36 -0500

    make location agnostic; clean up a bit

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,24 @@ use xml;
 use xml::reader::XmlEvent;
 
 fn main() {
-    println!("{}", parse_headline(get_news()));
+    println!("{}", parse_headline(get_news()).unwrap());
 }
 
 fn get_news() -> String {
-    reqwest::blocking::get("https://news.google.com/rss?hl=en-US&gl=US&ceid=US:en").unwrap().text().unwrap()
+    reqwest::blocking::get("https://news.google.com/rss").unwrap().text().unwrap()
 }
 
-fn parse_headline(news: String) -> String {
+fn parse_headline(news: String) -> Result<String, String> {
     let parser = xml::reader::EventReader::from_str(news.as_str());
     let mut found_item = false;
 
     for event in parser {
         match event.unwrap() {
-            XmlEvent::StartElement { name, attributes: _, namespace: _ } => found_item = found_item || name.local_name == "item",
-            XmlEvent::Characters(headline) => if found_item { return headline; },
+            XmlEvent::StartElement { name, attributes: _, namespace: _ } => if name.local_name == "item" { found_item = true },
+            XmlEvent::Characters(headline) => if found_item { return Ok(headline); },
             _ => {}
         };
     }
 
-    String::from("No headlines found.")
+    Err(String::from("No headlines found"))
 }
